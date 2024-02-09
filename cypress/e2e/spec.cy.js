b038e5ffe8f2796913eff3b0a8067eb131d83142
Henry <hnguyhenry@protonmail.com>
2024-02-08 22:21:20 -0800

    Add test for form component

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,38 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+  before(() => {
+    cy.visit('http://localhost:4321/');
+  });
+
+  it('Form should accept email and display correct error message otherwise', () => {
+    const formTest = () => {
+      // Get form component - input
+      cy.get('[data-test="form"]').as('form');
+      cy.get('@form').should('exist');
+
+      cy.get('[data-test="button"]').as('btn');
+      cy.get('@btn').should('exist');
+      cy.get('[data-test="error"]').should('not.exist');
+
+      // Test for correct email
+      cy.get('@form').type('test@email.com');
+      cy.get('@btn').click();
+      cy.get('[data-test="error"]').should('not.exist');
+
+      cy.get('@form').clear();
+
+      // Test for incorrect email
+      cy.get('@form').type('testtext');
+      cy.get('@btn').click();
+      cy.get('[data-test="error"]').should('exist');
+    };
+
+    // Test mobile viewport
+    cy.viewport('iphone-6');
+    formTest();
+
+    // Test desktop viewport
+    cy.viewport('macbook-16');
+    cy.reload();
+    formTest();
+  });
+});
